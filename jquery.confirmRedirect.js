3f4e71b262aeb29fa4732423efb17f34e0220005
withelmo <sugimoto@fusic.co.jp>
2013-01-11 19:24:40 +0900

    first commit

---
+++ jquery.confirmRedirect.js
@@ -0,0 +1,52 @@
+/**
+ * jQuery.ConfirmRedirect
+ * under MIT license.
+ *
+ * @projectDescription The confirmation before redirecting using jQuery.
+ * @author withelmo()
+ */
+
+(function($){
+
+    $.fn.confirmRedirect = function(option) {
+	var settings = $.extend({
+	    'message': 'このページから移動してもよろしいですか？',
+	    'url': '/',
+	    'data-msg-name': 'crmsg',
+	    'data-url-name': 'crurl'
+	}, option);
+
+	return this.each(function() {
+	    var href;
+	    href = $(this).attr('href');
+	    if (href !== '#') {
+		$(this).attr({
+		    'href': '#'
+		}).data(settings['data-url-name'], href);
+	    }
+
+	    $(this).on('click', function() {
+		var message, url;
+
+		// set message for popup
+		message = $(this).data(settings['data-msg-name']);
+		if (!message) {
+		    message = settings['message'];
+		}
+
+		// set URL for redirect
+		url = $(this).data(settings['data-url-name']);
+		if (!url) {
+		    url = settings['url'];
+		}
+
+		// confirm
+		if (confirm(message)) {
+		    location.href = url;
+		}
+		return false;
+	    });
+	});
+    };
+
+})($);
